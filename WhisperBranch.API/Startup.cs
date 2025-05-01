1428b96a3b663f6540fe995f21abf9058243e059
Ilyalobov <ilya.l.richard@gmail.com>
2025-05-01 21:21:54 +0300

    program cs to program and startap
    program cs to program and startap

---
+++ WhisperBranch.API/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace WhisperBranch.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WhisperBranch API V1");
+                    c.RoutePrefix = string.Empty; 
+                });
+                app.UseDeveloperExceptionPage();
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ WhisperBranch.Persistence/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhisperBranch.Persistence.Context;
+
+namespace WhisperBranch.Persistence.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<WBContext>(options =>
+                options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+
+           
+            return services;
+        }
+    }
+
+}
