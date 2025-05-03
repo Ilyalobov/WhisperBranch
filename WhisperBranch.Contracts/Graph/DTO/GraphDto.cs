e029371aa849ffd0c5f82237232f02bfa20e4850
Ilyalobov <ilya.l.richard@gmail.com>
2025-05-03 13:42:00 +0300

    add Graph Controller
    add Graph Controller

---
+++ WhisperBranch.Contracts/Graph/DTO/GraphDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WhisperBranch.Contracts.Graph
+{
+    public class GraphDto
+    {
+        public int Value { get; set; }
+
+        public required GraphDto[] Children { get; set; }
+    }
+}
